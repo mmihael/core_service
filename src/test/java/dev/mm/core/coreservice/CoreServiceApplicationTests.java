d11751b896ca13300e2ec1f18ae317b9e53880a8
mmihael <mihael.mamula@gmail.com>
2020-04-17 12:05:43 +0200

    starting point

---
+++ src/test/java/dev/mm/core/coreservice/CoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mm.core.coreservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
